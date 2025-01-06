82ae9c56f0bee92a0db2df79b73e00a94b68ce80
Yamato Matsumura <yamtheyam104@gmail.com>
2025-01-05 17:02:12 -0700

    Added more code to save_training_notes to clean up main

--- result_saving_utils.py
+++ result_saving_utils.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import date
 import tensorflow as tf
-import yaml
+from sklearn.metrics import mean_squared_error
 
-from config import SEQUENCE_LENGTH, N_DAYS, BATCH_SIZE
+from config import SEQUENCE_LENGTH, PREDICTION_WINDOW, BATCH_SIZE
 
 
 def create_new_dir(ticker):
@@ -20,13 +20,8 @@ def create_new_dir(ticker):
         dir_count += 1
     
     # Create directory to house this training session's data
-    custom_name = input("Custom Name for directory (Enter to skip): ")
-    if len(custom_name) != 0:
-        dir_path = f'data/{ticker}/{custom_name}'
-        os.makedirs(dir_path)
-    else:
-        dir_path = f'data/{ticker}/{dir_count}'
-        os.makedirs(dir_path)
+    dir_path = f'data/{ticker}/{dir_count}'
+    os.makedirs(dir_path)
 
     return dir_path
 
@@ -34,31 +29,25 @@ def save_model(model, dir_path):
     # Save keras model
     model.save(dir_path + '/model.keras')
 
-def create_results_graph(label_scaler, predictions, testing_labels):
+def create_results_graph(predicted_labels, stock_data):
+    actual_labels = stock_data.data['Close'].iloc[:1*PREDICTION_WINDOW].tolist()
 
-    # Undo scaling on predictions and labels
-    predictions = label_scaler.inverse_transform(predictions)
-    testing_labels = label_scaler.inverse_transform(testing_labels)
+    # Reverse since stock_data.data has most recent data first
+    actual_labels.reverse()
 
-    for i in range(len(testing_labels)):
-        # Initialize Graph
-        results_graph = plt.figure(figsize=(10, 6))
 
-        # Plot actual labels
-        plt.plot(testing_labels[i], linestyle='-', linewidth = 0.7, color='b', label='Actual', marker='o', markersize=1)
+    results_graph = plt.figure(figsize=(10, 6))
 
-        # Plot predictions
-        plt.plot(predictions[i], linestyle='--', linewidth = 0.7, color='r', label='Predicted', marker='o', markersize=1)
+    plt.plot(list(actual_labels), linestyle='-', linewidth = 0.7, color='b', label='Actual', marker='o', markersize=1)
+    plt.plot(list(predicted_labels[0]), linestyle='--', linewidth = 0.7, color='r', label='Predicted', marker='o', markersize=1)
+    plt.title('Actual vs. Predicted')
+    plt.xlabel('Sample')
+    plt.ylabel('Value')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
 
-        # Customize plot
-        plt.title('Actual vs. Predicted')
-        plt.xlabel('Sample')
-        plt.ylabel('Value')
-        plt.legend()
-        plt.grid(True)
-        plt.tight_layout()
-
-        return results_graph
+    return results_graph
     
 def save_graph(graph, dir_path, file_name):
         graph.savefig(f'{dir_path}/{file_name}')
@@ -80,14 +69,26 @@ def create_loss_graph(history):
     return loss_graph
 
 
-def save_training_notes(dir_path, model, best_val_loss, r_value, rmse):
+def save_training_notes(dir_path, model, history, predicted_labels, data):
+    # Grab testing labels from stock_data.data
+    testing_labels = data['Close'].iloc[:1*PREDICTION_WINDOW].tolist()
+    testing_labels.reverse()
+
+    # Reshape predictions into 1D
+    predicted_labels = np.array(predicted_labels).flatten()
+
+    val_loss = history.history['loss']
+    best_val_loss = min(val_loss)
+    r_value = np.corrcoef(testing_labels, predicted_labels)[0, 1]
+    rmse = np.sqrt(mean_squared_error(testing_labels, predicted_labels))
+
     with open(f'{dir_path}/notes.txt', 'w') as file:
         file.write('='*17 + ' ' + "[Model Summary]" + ' ' + '='*18 + '\n')
         file.write(f"Date: {str(date.today().strftime('%m/%d/%Y'))} \n \n \n")
 
         file.write('='*16 + ' ' + "[Model Parameters]" + ' ' + '='*16 + '\n')
         file.write("- Sequence Length: " + str(SEQUENCE_LENGTH) + '\n')
-        file.write("- Predicting N Days: " + str(N_DAYS) + '\n')
+        file.write("- Predicting N Days: " + str(PREDICTION_WINDOW) + '\n')
         file.write("- Batch Size: " + str(BATCH_SIZE) + '\n')
         file.write('='*52 + '\n \n')
 
