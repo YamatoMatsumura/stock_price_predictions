b16c191d505e62dd4d9d4ea1183f3a7d327321e8
Yamato Matsumura <yamtheyam104@gmail.com>
2025-05-11 21:30:40 -0500

    Adjusted start balance for simulation

--- simulation_globals.py
+++ simulation_globals.py
@@ -10,4 +10,4 @@ TRADING_CLIENT = TradingClient(KEY, SECRET, paper=True)
 SIMULATED_TIME = datetime.datetime(2023,1,3) # Simulated time for testing
 BUY_ORDERS = {} # Dictionary to hold buy orders
 PORTFOLIO = [] # List to hold stocks in portfolio
-BALANCE = 1000000 # Starting balance for simulation
+BALANCE = 3000 # Starting balance for simulation
